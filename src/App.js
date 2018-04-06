c81f124dd255d080ed7d02096c8e0cfa5f608ae5
aungzanbaw <aungzanbaw@gmail.com>
2018-04-06 12:27:06 +0630

    BooksList are working now

--- src/App.js
+++ src/App.js
@@ -7,20 +7,35 @@ import ListBook from './ListBook'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll()
+      .then(books => {
+        this.setState({ books })
+      })
+  }
+
+  updateBook(book, shelf){
+    
+    this.setState((state) => ({
+      // books: state.books.filter((b) => {
+      //   if(b.id === book.id)
+      //     console.log(b,book)
+      // })
+    }));
+
+    BooksAPI.update(book, shelf)
+      .then(books => console.log(books))
+      
   }
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={()=> (
-          <ListBook />
+          <ListBook books={ this.state.books }/>
         )} />
 
         <Route path="/search" render={()=> (
--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -12,6 +12,11 @@ const headers = {
   'Authorization': token
 }
 
+// export const status = () => 
+//   fetch(`${api}/status`, { headers })
+//   .then(res => res.json())
+//   .then(data => data)
+
 export const get = (bookId) =>
   fetch(`${api}/books/${bookId}`, { headers })
     .then(res => res.json())
